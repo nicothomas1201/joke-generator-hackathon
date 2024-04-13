4105a92ca13a1abdbe662a1f3bb2fef976c98ee4
Nicolas Quintero <quinterorodrigueznicolas@gmail.com>
2024-04-13 09:56:50 -0500

    add ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
-import react from '@astrojs/react'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   devToolbar: {
-    enabled: false,
+    enabled: false
   },
   integrations: [react()],
-})
+  adapter: vercel()
+});
