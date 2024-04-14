a0549f5f2941fcdb490c85cc01f9e859b6653f64
Yeider <yeiderjp12@gmail.com>
2024-04-14 02:06:26 -0500

    added  hover and loading animation in the generate button

---
+++ src/icons/Loading.tsx
@@ -0,0 +1,11 @@
+import '@/styles/loading.css'
+
+export function Loading () {
+  return (
+    <div className="container">
+      <div className="circle1"> </div>
+      <div className="circle2"> </div>
+      <div className="circle3"> </div>
+    </div>
+  )
+}
