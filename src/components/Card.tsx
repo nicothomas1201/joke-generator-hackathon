d9144ccaf8d1df25ae266fc09d5a713db0e1a604
Group4First <Group4First@gmail.com>
2024-04-14 15:43:47 -0500

    added prop scale in card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,15 +3,16 @@ import '@/styles/card.css'
 interface CardProps {
   left?: string
   top?: string
+  scale?: string
   rotation?: string
   children?: React.ReactNode
 }
 
-export function Card({ left = '10%', top = '50%', rotation = '-16deg', children }: CardProps) {
+export function Card({ scale = '100%', left = '10%', top = '50%', rotation = '-16deg', children }: CardProps) {
   return (
-    <div className="card" style={{ left, top, transform: `rotate(${rotation})` }}>
+    <div className="card" style={{ left, top, transform: `rotate(${rotation}) scale(${scale})`}}>
       {children}
-      <span> algunos lor lorempin su sum lorem loremiptsum </span>
+      <span> lorempin lor lorempin su sum lorem loremiptsum </span>
     </div>
   )
 }
